569608ca1ad039608c989f72390e7a656ef51616
庞振硕 <616358428@qq.com>
2019-02-20 10:44:16 +0800

    引入element-ui

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import App from './App'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
