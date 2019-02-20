dd2076a410541462562cb863bad3ca029d8d3b21
庞振硕 <616358428@qq.com>
2019-02-20 11:32:49 +0800

    引入resetcss、添加Header组件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
     <div id="app">
+        <pet-header></pet-header>
     </div>
 </template>
 
 <script>
+    import Header from './components/Header'
+
     export default {
-        name: 'App'
+        name: 'App',
+        components: {
+            'pet-header': Header
+        }
     }
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="header">
+        <div class="menu-wrapper">
+            <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect"
+                     background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+                <el-menu-item index="1">首页</el-menu-item>
+                <el-menu-item index="2">领养</el-menu-item>
+                <el-menu-item index="3">助养</el-menu-item>
+                <el-menu-item index="4">知识</el-menu-item>
+                <el-menu-item index="5">百科</el-menu-item>
+                <el-menu-item index="6">秀宠回访</el-menu-item>
+            </el-menu>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        data() {
+            return {
+                activeIndex: '1'
+            }
+        },
+        methods: {
+            handleSelect(key, keyPath) {
+                console.log(key, keyPath)
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .header
+        width 100%
+        background-color #545c64
+        .menu-wrapper
+            width 1140px
+            margin 0 auto
+</style>
