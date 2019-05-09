f9da7a7652e585e475bb1a02a32efb9c470c7972
庞振硕 <616358428@qq.com>
2019-05-09 10:48:17 +0800

    领养组件页面完成

--- src/modules/adopt/Adopt.vue
+++ src/modules/adopt/Adopt.vue
@@ -1,6 +1,131 @@
 <template>
     <div class="adopt">
-        领养组件
+        <div class="content-left">
+            <div class="banner">
+                <div class="img-wrapper">
+                    <img src="http://www.petly.net/data/attachment/block/1e/1e16d0dcd96b9e3a6a061781c0e440b9.jpg">
+                </div>
+                <div class="content-wrapper">
+                    <div class="top">
+                        <h1 class="title">两岁母猫求主人，已绝育</h1>
+                        <p class="description">我们家猫咪是成年猫咪，希望找个爱猫朋友领养喵喵[握手]（猫咪资料：成年母猫猫，大概</p>
+                    </div>
+                    <div class="bottom">
+                        <ul class="list">
+                            <li class="list-item"><a href="javascript:;">乐清地区求领养一只橘猫 能养到压塌炕</a></li>
+                            <li class="list-item"><a href="javascript:;">乐清地区求领养一只橘猫 能养到压塌炕</a></li>
+                            <li class="list-item"><a href="javascript:;">乐清地区求领养一只橘猫 能养到压塌炕</a></li>
+                            <li class="list-item"><a href="javascript:;">乐清地区求领养一只橘猫 能养到压塌炕</a></li>
+                            <li class="list-item"><a href="javascript:;">乐清地区求领养一只橘猫 能养到压塌炕</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="province">
+                <h1 class="title">宠物领养</h1>
+                <div class="province-list">
+                    <div class="list-item">
+                        <div class="img-wrapper">
+                            <img src="http://www.petly.net/data/attachment/common/c8/common_2_icon.png">
+                        </div>
+                        <div class="content-wrapper">
+                            <h1 class="province-name">北京宠物领养</h1>
+                            <p class="description">北京免费领养宠物的网站</p>
+                            <div class="count-wrapper">
+                                <span>主题：5980</span>
+                                <span>帖数：10000</span>
+                            </div>
+                            <p class="date">最后发表：昨天 18:00</p>
+                        </div>
+                    </div>
+                    <div class="list-item">
+                        <div class="img-wrapper">
+                            <img src="http://www.petly.net/data/attachment/common/c8/common_2_icon.png">
+                        </div>
+                        <div class="content-wrapper">
+                            <h1 class="province-name">北京宠物领养</h1>
+                            <p class="description">北京免费领养宠物的网站</p>
+                            <div class="count-wrapper">
+                                <span>主题：5980</span>
+                                <span>帖数：10000</span>
+                            </div>
+                            <p class="date">最后发表：昨天 18:00</p>
+                        </div>
+                    </div>
+                    <div class="list-item">
+                        <div class="img-wrapper">
+                            <img src="http://www.petly.net/data/attachment/common/c8/common_2_icon.png">
+                        </div>
+                        <div class="content-wrapper">
+                            <h1 class="province-name">北京宠物领养</h1>
+                            <p class="description">北京免费领养宠物的网站</p>
+                            <div class="count-wrapper">
+                                <span>主题：5980</span>
+                                <span>帖数：10000</span>
+                            </div>
+                            <p class="date">最后发表：昨天 18:00</p>
+                        </div>
+                    </div>
+                    <div class="list-item">
+                        <div class="img-wrapper">
+                            <img src="http://www.petly.net/data/attachment/common/c8/common_2_icon.png">
+                        </div>
+                        <div class="content-wrapper">
+                            <h1 class="province-name">北京宠物领养</h1>
+                            <p class="description">北京免费领养宠物的网站</p>
+                            <div class="count-wrapper">
+                                <span>主题：5980</span>
+                                <span>帖数：10000</span>
+                            </div>
+                            <p class="date">最后发表：昨天 18:00</p>
+                        </div>
+                    </div>
+                    <div class="list-item">
+                        <div class="img-wrapper">
+                            <img src="http://www.petly.net/data/attachment/common/c8/common_2_icon.png">
+                        </div>
+                        <div class="content-wrapper">
+                            <h1 class="province-name">北京宠物领养</h1>
+                            <p class="description">北京免费领养宠物的网站</p>
+                            <div class="count-wrapper">
+                                <span>主题：5980</span>
+                                <span>帖数：10000</span>
+                            </div>
+                            <p class="date">最后发表：昨天 18:00</p>
+                        </div>
+                    </div>
+                    <div class="list-item">
+                        <div class="img-wrapper">
+                            <img src="http://www.petly.net/data/attachment/common/c8/common_2_icon.png">
+                        </div>
+                        <div class="content-wrapper">
+                            <h1 class="province-name">北京宠物领养</h1>
+                            <p class="description">北京免费领养宠物的网站</p>
+                            <div class="count-wrapper">
+                                <span>主题：5980</span>
+                                <span>帖数：10000</span>
+                            </div>
+                            <p class="date">最后发表：昨天 18:00</p>
+                        </div>
+                    </div>
+                    <div class="list-item">
+                        <div class="img-wrapper">
+                            <img src="http://www.petly.net/data/attachment/common/c8/common_2_icon.png">
+                        </div>
+                        <div class="content-wrapper">
+                            <h1 class="province-name">北京宠物领养</h1>
+                            <p class="description">北京免费领养宠物的网站</p>
+                            <div class="count-wrapper">
+                                <span>主题：5980</span>
+                                <span>帖数：10000</span>
+                            </div>
+                            <p class="date">最后发表：昨天 18:00</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content-right"></div>
     </div>
 </template>
 
@@ -11,5 +136,108 @@
 </script>
 
 <style lang="stylus" scoped>
-
+    .adopt
+        display flex
+        justify-content space-between
+        flex-wrap wrap
+        width 1140px
+        margin 30px auto
+        .content-left
+            width 817px
+            box-sizing border-box
+            .banner
+                display flex
+                justify-content space-between
+                width 100%
+                box-sizing border-box
+                padding 20px
+                border 1px solid #e6e6e6
+                background-color #fff
+                .img-wrapper
+                    width 450px
+                    height 285px
+                    img
+                        width inherit
+                        height inherit
+                .content-wrapper
+                    width 305px
+                    .top
+                        padding-bottom 15px
+                        margin-bottom 10px
+                        border-bottom 1px solid #e6e6e6
+                        .title
+                            height 30px
+                            margin-bottom 5px
+                            line-height 30px
+                            font-size 18px
+                            color #333
+                        .description
+                            line-height 22px
+                            font-size 12px
+                            color #888
+                    .bottom
+                        .list
+                            .list-item
+                                display flex
+                                align-items center
+                                width 265px
+                                height 35px
+                                a
+                                    font-size 13px
+                                    color #333
+            .province
+                width 100%
+                box-sizing border-box
+                padding 10px 20px
+                border 1px solid #e6e6e6
+                margin-top 20px
+                background-color #fff
+                .title
+                    width 100%
+                    height 50px
+                    line-height 50px
+                    font-size 16px
+                    font-weight 600
+                    color #333
+                    border-bottom 1px solid #e6e6e6
+                .province-list
+                    display flex
+                    justify-content space-between
+                    flex-wrap wrap
+                    .list-item
+                        display flex
+                        width 258px
+                        height 124px
+                        box-sizing border-box
+                        padding 20px 10px 20px 0
+                        .img-wrapper
+                            width 60px
+                            height 60px
+                            img
+                                width inherit
+                                height inherit
+                        .content-wrapper
+                            margin-left 10px
+                            .province-name
+                                line-height 24px
+                                font-size 16px
+                                font-weight 100
+                                color #333
+                            .description
+                                line-height 18px
+                                font-size 12px
+                                color #888
+                            .count-wrapper
+                                line-height 18px
+                                font-size 12px
+                                color #363636
+                            .date
+                                line-height 18px
+                                font-size 12px
+                                color #666
+        .content-right
+            width 302px
+            box-sizing border-box
+            border 1px solid #e6e6e6
+            background-color: #fff;
 </style>
