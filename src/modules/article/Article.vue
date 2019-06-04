b207a3433d6288debd1490eaf2db81ebe5243c60
庞振硕 <616358428@qq.com>
2019-06-05 00:29:11 +0800

    修改广告

--- src/modules/article/Article.vue
+++ src/modules/article/Article.vue
@@ -13,7 +13,50 @@
             </div>
             <div class="content-wrapper" v-html="article.content"></div>
         </div>
-        <div class="content-right"></div>
+        <div class="content-right">
+            <div class="class-wrapper">
+                <div class="show">
+                    <a href="http://www.cpse-expo.com" target="_blank">
+                        <img src="../../../static/img/show.jpg">
+                    </a>
+                </div>
+            </div>
+            <div class="latest-title">
+                最新主题
+            </div>
+            <div class="latest-theme-wrapper">
+                <div class="img-info">
+                    <div class="info-detail">
+                        <img src="http://www.petly.net/data/attachment/common/a5/common_38_icon.png" alt="">
+                        <p><a href="#">4个月小白猫</a></p>
+                    </div>
+                    <div class="info-detail">
+                        <img
+                            src="https://pet-1254154566.cos.ap-chengdu.myqcloud.com/23a5fba5-4ce0-48bb-9305-e28e21e74a18.jpg"
+                            alt="">
+                        <p><a href="#">一岁半萨摩耶寻主</a></p>
+                    </div>
+                </div>
+                <ul>
+                    <li><a href="javascript:;">（上海徐汇区）家里的母猫生了4只小猫希望</a></li>
+                    <li><a href="javascript:;">免费领养4年法斗</a></li>
+                    <li><a href="javascript:;">4个月金毛 好心人领养</a></li>
+                </ul>
+            </div>
+            <div class="latest-title">
+                领养推荐
+            </div>
+            <div class="latest-theme-wrapper">
+                <ul>
+                    <li><a href="javascript:;">浦东陆家嘴附近猫咪领养</a></li>
+                    <li><a href="javascript:;">4年雪橇犬</a></li>
+                    <li><a href="javascript:;">6个月阿拉斯加 好心人领养</a></li>
+                    <li><a href="javascript:;">上海虹桥附近猫咪领养</a></li>
+                    <li><a href="javascript:;">北京西红门附近猫咪领养</a></li>
+                    <li><a href="javascript:;">北京大兴附近猫咪领养</a></li>
+                </ul>
+            </div>
+        </div>
     </article>
 </template>
 
@@ -54,7 +97,7 @@
         display flex
         justify-content space-between
         width 1140px
-        margin 25px auto 0
+        margin 30px auto
         .content-left
             width 812px
             box-sizing border-box
@@ -94,4 +137,52 @@
             padding 0 20px
             border 1px solid #e6e6e6
             background-color #fff
+            .show
+                width 260px
+                height 180px
+                img
+                    width 100%
+                    height 100%
+            .latest-title
+                height: 50px
+                border-bottom 1px solid #e8e8e8
+                margin-bottom 15px
+                line-height 50px
+                font-size 18px
+                color #333
+            .class-wrapper
+                display flex
+                flex-wrap wrap
+                padding 10px 0
+                margin-bottom 15px
+            .latest-theme-wrapper
+                .img-info
+                    display flex
+                    justify-content space-between
+                    .info-detail
+                        width 140px
+                        margin-bottom 10px
+                        img
+                            width 110px
+                            &:hover
+                                cursor pointer
+                        a
+                            font-size 13px
+                            color #333333
+                            &:hover
+                                color #409EFF
+                li
+                    a
+                        display inline-block
+                        overflow hidden
+                        width 260px
+                        height 40px
+                        line-height 40px
+                        font-size 13px
+                        color #333333
+                        white-space nowrap
+                        text-overflow ellipsis
+                        &:hover
+                            color #409EFF
+                            text-decoration underline
 </style>
